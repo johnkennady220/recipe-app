c3a533bbf080d97fa9979a697cf4cfed2f5f60c1
johnkennady220 <johnkennady220@gmail.com>
2023-11-17 21:13:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
+import SearchBar from "./components/SearchBar";
+import RecipeCard from "./components/RecipeCard";
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 
 function App() {
+  const [isLoading,setIsLoading] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [query, setQuery] = useState("");
+
+   //function to search for the recipes
+
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url =apiUrl + query;
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data);
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes();
+  },[]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2> Our Food Recipes</h2>
+      <SearchBar
+        isLoading={isLoading}
+        query={query}
+        setQuery={setQuery}
+        handleSubmit={handleSubmit}
+      />
+      <div className="recipes">
+      {recipes ? recipes.map(recipe => (
+          <RecipeCard
+             key={recipe.idMeal}
+             recipe={recipe}
+          />
+           ))
+        :"No Recipes!"}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
